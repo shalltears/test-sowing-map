bf4186382e62152be6e70ae8a6b99792aeda4a31
shalltears <910263965@qq.com>
2018-11-15 11:27:29 +0800

    first submit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var mainImg = document.getElementsByClassName('main-img')[0];
+
+var timer = setInterval(function(){
+    move();
+},2000)
+var timer1 = setInterval(function(){
+    move1();
+},10)
+
+function move(){
+    var mainIMgLeft = parseInt(mainImg.style.left);
+    mainImg.style.left = mainIMgLeft - 400 + 'px';
+}
+function move1(){
+    var mainIMgLeft = parseInt(mainImg.style.left);
+    if(mainIMgLeft == -2000){
+        mainImg.style.transitionDuration = '0s';
+        mainImg.style.left = '0px';
+    }
+    if(mainIMgLeft == 0){
+        mainImg.style.transitionDuration = '1s';
+        mainImg.style.left = '-400px';
+    }
+}
