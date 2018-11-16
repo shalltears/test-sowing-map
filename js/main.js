98d2ac3f2444a09f053ab205675e72b6ddfe2d73
shalltear <910263965@qq.com>
2018-11-16 17:39:11 +0800

    去掉一个定时器，改用延时完成回滚操作。

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
 var mainImg = document.getElementsByClassName('main-img')[0];
-
-var timer = setInterval(function(){
+var timer = setInterval(function () {
     move();
-},2000)
-var timer1 = setInterval(function(){
-    move1();
-},10)
-
-function move(){
+}, 3000);
+function move() {
     var mainIMgLeft = parseInt(mainImg.style.left);
-    mainImg.style.left = mainIMgLeft - 500 + 'px';
+    if (mainIMgLeft == -2000) {
+        back();
+    }else {
+        mainImg.style.left = mainIMgLeft - 500 + 'px';
+    }
 }
-function move1(){
+function back() {
     var mainIMgLeft = parseInt(mainImg.style.left);
-    if(mainIMgLeft == -2500){
-        mainImg.style.transitionDuration = '0s';
-        mainImg.style.left = '0px';
-    }
-    if(mainIMgLeft == 0){
+    mainImg.style.transitionDuration = '0s';
+    mainImg.style.left = '0px';
+    mainIMgLeft = 0;
+    setTimeout(function(){
         mainImg.style.transitionDuration = '1s';
-        mainImg.style.left = '-500px';
-    }
+        mainImg.style.left = mainIMgLeft - 500 + 'px';
+    },1)
+}
+
+var btn1 = document.getElementsByClassName('btn-1')[0];
+var btn2 = document.getElementsByClassName('btn-2')[0];
+btn1.onclick = function () {
+    move();
+}
+btn2.onclick = function () {
+    move();
 }
