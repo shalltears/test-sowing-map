6fb7d0c594c79b2447f5c4c3847eb4b0e253321b
shalltears <910263965@qq.com>
2018-11-18 14:48:46 +0800

    全部重构（解决了动画问题）

--- js/main.js
+++ js/main.js
@@ -1,31 +1,30 @@
-var mainImg = document.getElementsByClassName('main-img')[0];
-var timer = setInterval(function () {
-    move();
-}, 3000);
-function move() {
-    var mainIMgLeft = parseInt(mainImg.style.left);
-    if (mainIMgLeft == -2000) {
-        back();
-    }else {
-        mainImg.style.left = mainIMgLeft - 500 + 'px';
+var obj = {
+    sleepTime: 2000,//轮播延时
+    cont: 0,//第几张
+    origin: document.getElementsByClassName('main-img')[0],//拿到全部图片的父级
+    init: function () {
+        this.timer();
+    },
+    move: function (origin) {
+        this.cont++;
+        clearInterval(timer1);
+        origin.style.transform = 'translate3d(' + this.cont * (-500) + 'px,0px,0px)';
+        origin.style.transitionDuration = '1s';
+        this.sleepTime = 2000;
+        this.timer();
+        if (this.cont > 4) {
+            clearInterval(timer1);
+            this.cont = 0;
+            origin.style.transitionDuration = '0s';
+            origin.style.transform = 'translate3d(' + this.cont * (-500) + 'px,0px,0px)';
+            this.sleepTime = 0;
+            this.timer();
+        }
+    },
+    timer: function () {
+        timer1 = setInterval(function () {
+            obj.move(obj.origin);
+        }, obj.sleepTime)
     }
 }
-function back() {
-    var mainIMgLeft = parseInt(mainImg.style.left);
-    mainImg.style.transitionDuration = '0s';
-    mainImg.style.left = '0px';
-    mainIMgLeft = 0;
-    setTimeout(function(){
-        mainImg.style.transitionDuration = '1s';
-        mainImg.style.left = mainIMgLeft - 500 + 'px';
-    },1)
-}
-
-var btn1 = document.getElementsByClassName('btn-1')[0];
-var btn2 = document.getElementsByClassName('btn-2')[0];
-btn1.onclick = function () {
-    move();
-}
-btn2.onclick = function () {
-    move();
-}
+obj.init();
