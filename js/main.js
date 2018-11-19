b2df67b3923087464fcd9607aef162735a64ee3f
shalltear <910263965@qq.com>
2018-11-19 15:53:00 +0800

    增加左键点击优化，基本实现所有功能（还剩：点完左键后偶尔还会往左走一步（state待优化））

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var obj = {
     sleepTime: 2000,//轮播延时
     cont: 0,//第几张
     step: -500,//步距
+    state: true,//左为false，右为true
     origin: document.getElementsByClassName('main-img')[0],//拿到全部图片的父级
     btnLeft: document.getElementsByClassName('btn')[0],
     btnRight: document.getElementsByClassName('btn')[1],
@@ -9,15 +10,23 @@ var obj = {
         this.timer();
     },
     move: function (origin) {//移动
-        this.cont++;
+        this.state == true ? this.cont++ : this.cont--;
         clearInterval(timer1);
         origin.style.transform = 'translate3d(' + this.cont * this.step + 'px,0px,0px)';
         origin.style.transitionDuration = '0.3s';
         this.sleepTime = 2000;
         this.timer();
-        if (this.cont > 4 && this.step !== 0) {
+        if (this.cont > 4 && this.state == true) {
             clearInterval(timer1);
             this.cont = 0;
+            origin.style.transitionDuration = '0s';
+            origin.style.transform = 'translate3d(' + this.cont * this.step + 'px,0px,0px)';
+            this.sleepTime = 50;
+            this.timer();
+        }
+        if (this.cont < 0 && this.state == false) {
+            clearInterval(timer1);
+            this.cont = 4;
             origin.style.transitionDuration = '0s';
             origin.style.transform = 'translate3d(' + this.cont * this.step + 'px,0px,0px)';
             this.sleepTime = 50;
@@ -27,17 +36,16 @@ var obj = {
     timer: function () {//计时器
         timer1 = setInterval(function () {
             obj.move(obj.origin);
+            obj.state = true;
         }, obj.sleepTime);
     }
 }
 obj.init();
 obj.btnLeft.onclick = function () {//向左
-    obj.cont -= 2;
-    if (obj.cont < 0 && this.step !== 0) {
-        obj.cont = 3;
-    }
+    obj.state = false;
     obj.move(obj.origin);
 }
 obj.btnRight.onclick = function () {//向右
+    obj.state = true;
     obj.move(obj.origin);
 }
